2961f2b9eae4fa03e2668258be7bcc214713ad58
kreempuff <10726831+kreempuff@users.noreply.github.com>
2025-02-22 13:40:53 -0500

    feat: add some useful network outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
 output "connection_command" {
   value = "gcloud container clusters get-credentials ${google_container_cluster.main.name} --zone ${var.cluster_location}"
 }
+
+output "network_name" {
+  value = google_compute_network.main.name
+}
+
+output "network_id" {
+  value = google_compute_network.main.id
+}
+
+output "network_control_nodes_subnet_id" {
+  value = google_compute_subnetwork.control_nodes.id
+}
+
+output "network_worker_nodes_subnet_ids" {
+  description = "A map of prefixed subnet names to their IDs"
+  value       = {for k, v in google_compute_subnetwork.worker_nodes : "${var.network_name}-${k}" => v.id}
+}
