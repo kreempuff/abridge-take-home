27e4928ccd313238655e98547ecea14528b0d8aa
kreempuff <10726831+kreempuff@users.noreply.github.com>
2025-02-21 19:18:53 -0500

    refactor: move networking resources up to main module
    Since I don't have a clear idea of how the networking resources will be
    used, I'm moving them up to the main module for now as I test out how to
    to configure GKE. The subtleties of GCP networking and cluster configuration
    is making it hard to iterate

---
+++ networking.tf
@@ -0,0 +1,20 @@
+resource "google_compute_network" "main" {
+  name = var.network_name
+
+  // TODO: Make this configurable
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "control_nodes" {
+  name          = "${var.network_name}-control-nodes"
+  network       = google_compute_network.main.id
+  ip_cidr_range = var.network_control_nodes_cidr
+}
+
+resource "google_compute_subnetwork" "worker_nodes" {
+  for_each      = var.network_worker_node_subnets
+  name          = "${var.network_name}-${each.key}"
+  network       = google_compute_network.main.id
+  ip_cidr_range = each.value.cidr
+  region        = each.value.region
+}
