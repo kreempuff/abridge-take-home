05be253f83e086e0a525a31707a0492ef632f974
kreempuff <10726831+kreempuff@users.noreply.github.com>
2025-02-23 12:45:54 -0500

    fix: add missing networking resources
    For private nodes in a cluster a router, nat and firewall are required to allow
    communication between the nodes and the internet.
    
    Testing with:
    - `kubectl run -it --rm --image ubuntu bash`
    - `apt update`
    
    If there's no access then the command will fail with a timeout.

--- networking.tf
+++ networking.tf
@@ -5,6 +5,31 @@ resource "google_compute_network" "main" {
   auto_create_subnetworks = false
 }
 
+resource "google_compute_router" "router" {
+  name    = "${var.network_name}-router"
+  network = google_compute_network.main.id
+  region  = var.network_control_nodes_region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.network_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+resource "google_compute_firewall" "main" {
+  name      = "${var.network_name}-internet-access"
+  network   = google_compute_network.main.id
+  direction = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "all"
+  }
+}
+
+
 resource "google_compute_subnetwork" "control_nodes" {
   name          = "${var.network_name}-control-nodes"
   network       = google_compute_network.main.id
